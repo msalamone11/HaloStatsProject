170ffb622b2027b41071375dee78c0a205a970d0
Mark Salamone <msalamone11@gmail.com>
2019-12-22 11:32:52 -0500

    Merged and removed extra files

--- HaloStatsProject/Pages/Index.cshtml.cs
+++ HaloStatsProject/Pages/Index.cshtml.cs
@@ -9,10 +9,23 @@ using HaloStatsProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+<<<<<<< Updated upstream
 using Newtonsoft.Json;
 
 namespace HaloStatsProject.Pages {
     public class IndexModel : PageModel {
+=======
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HaloStatsProject.Models;
+using Newtonsoft.Json;
+using HaloEzAPI;
+
+namespace HaloStatsProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+>>>>>>> Stashed changes
         private readonly ILogger<IndexModel> _logger;
         public RootObject PlayerStats;
 
@@ -20,6 +33,7 @@ namespace HaloStatsProject.Pages {
             _logger = logger;
         }
 
+<<<<<<< Updated upstream
         public void OnGet () {
             var client = new HttpClient ();
             client.BaseAddress = new Uri ("https://www.haloapi.com/");
@@ -34,6 +48,24 @@ namespace HaloStatsProject.Pages {
         public RedirectToPageResult OnPost () {
             var GamerTag = Request.Form["GamerTag"];
             return RedirectToPage ("Player", new { GamerTag = GamerTag });
+=======
+        public void OnGet()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.haloapi.com/");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "bdd5cbaf0ca34682a8f6b50742747e6f");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            string result = client.GetStringAsync("stats/h5/arena/matches/1d8e6578-947e-4855-a86f-85a014a748df").Result;
+            PlayerStats = JsonConvert.DeserializeObject<RootObject>(result);
+            PlayerStats.PlayerStats.OrderBy(player => player.TeamId).OrderBy(player => player.Rank);
+            DataTable matchDataTable = new DataTable();
+        }
+
+        public RedirectToPageResult OnPost()
+        {
+            var GamerTag = Request.Form["GamerTag"];
+            return RedirectToPage("Player", GamerTag);
+>>>>>>> Stashed changes
         }
     }
 }
